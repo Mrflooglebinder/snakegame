fb2b9128e78f2ccf6fb33d3357dcf83f7173849e
Floogle <mrflooglebinder@gmail.com>
2022-08-02 19:38:59 +0800

    Created snake.js file
    Moved the snake speed var from game..js
    and put it in snake.js and set it to export.
    - updated game.js to import the snake speed...
    
    Created snake.js file to denote all things for the snake
    Created 2 funtions for updating the snake, and to draw the snake.
    Set them to export and updated the import
    on game.js to include them both.
    
    Created funtions on game.js to call the
    funtions from snake.js both functions are
    basically empty just printing text to the
    console at the moment.

--- game.js
+++ game.js
@@ -1,16 +1,27 @@
+import { update as updateSnake, draw as drawSnake, SNAKE_SPEED } from './snake.js'
 
 let lasRenderTime = 0
-const SNAKE_SPEED = 1
 
 function main(currentTime) {
     window.requestAnimationFrame(main)
     const secondsSinceLsdtRender = (currentTime - lasRenderTime) / 1000
     if (secondsSinceLsdtRender <1 / SNAKE_SPEED) return
     
-    // console.log(currentTime)
-    console.log('screen rendered after a delay of ' + secondsSinceLsdtRender + ' seconds')
     lasRenderTime = currentTime
+    // console.log(currentTime)
+    //console.log('screen rendered after a delay of ' + secondsSinceLsdtRender + ' seconds')
+
+    update()
+    draw()
 
 }
 
 window.requestAnimationFrame(main)
+
+function update() {
+    updateSnake()
+}
+
+function draw() {
+    drawSnake()
+}
