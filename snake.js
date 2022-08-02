58f0bcbca5b0df399d7a8be05adfbab1fc548fd9
Floogle <mrflooglebinder@gmail.com>
2022-08-02 20:14:57 +0800

    Created Static Movement
    Set a few more array positions to lengthen
    the snake body.  Created a loop to update
    the array positions to move the snake.
    Fixed the x and y gridrow and grid col start
    positions as they were wrong.
    Commented out the console log text.
    Added game.js draw function to remove the innerHTML - this removes the snakes
    tail so it doesnt just continuosly grow.

--- snake.js
+++ snake.js
@@ -1,19 +1,32 @@
 export const SNAKE_SPEED = 1
-const snakeBody = [{ x: 11, y: 11 }]
+const snakeBody = [
+    { x: 10, y: 11 },
+    { x: 11, y: 11 },
+    { x: 12, y: 11 },
+    { x: 13, y: 11 },
+    { x: 14, y: 11 },
+    { x: 15, y: 11 }
+]
 
 export function update() {
-    console.log('Update Snake')
+    //console.log('Update Snake')
+    for (let i = snakeBody.length -2; i >= 0; i--) {
+        snakeBody[i + 1] = {...snakeBody[i]}
+    }
+
+    snakeBody[0].x += 0
+    snakeBody[0].y += 1
     
 }
 
 export function draw(gameBoard) {
-    console.log('Draw Snake')
+    //console.log('Draw Snake')
     snakeBody.forEach(segment => {
         const snakeElement = document.createElement('div')
-        snakeElement.style.gridRowStart = segment.x
-        snakeElement.style.gridColumnStart = segment.y
+        snakeElement.style.gridRowStart = segment.y
+        snakeElement.style.gridColumnStart = segment.x
         snakeElement.classList.add('snake')
         gameBoard.appendChild(snakeElement)
-        
+
     })
 }
