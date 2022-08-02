9505a92da60dc86e8489300b0434180a9ec0cf17
Floogle <mrflooglebinder@gmail.com>
2022-08-02 20:35:46 +0800

    Create user Inputs
    Created file input.js with var inputDirection
    with array set to 0 0
    Created a windows event listener for key
    down and set a switch staement to define
    case statements for arrow up, down, L, R.
    
    Updated snake.js to remove the bulk of the
    snake array (snakes body) to display just
    the snake head again.
    updated the static values in snake body to use inputDirection. x and y..
    Bumped up the snake speed to have a play.

--- snake.js
+++ snake.js
@@ -1,21 +1,17 @@
-export const SNAKE_SPEED = 1
-const snakeBody = [
-    { x: 10, y: 11 },
-    { x: 11, y: 11 },
-    { x: 12, y: 11 },
-    { x: 13, y: 11 },
-    { x: 14, y: 11 },
-    { x: 15, y: 11 }
-]
+import { getInputDirection } from "./input.js"
+
+export const SNAKE_SPEED = 25
+const snakeBody = [{ x: 11, y: 11 }]
 
 export function update() {
     //console.log('Update Snake')
+    const inputDirection = getInputDirection()
     for (let i = snakeBody.length -2; i >= 0; i--) {
         snakeBody[i + 1] = {...snakeBody[i]}
     }
 
-    snakeBody[0].x += 0
-    snakeBody[0].y += 1
+    snakeBody[0].x += inputDirection.x
+    snakeBody[0].y += inputDirection.y
     
 }
 
